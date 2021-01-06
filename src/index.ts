008980dca1e2b8b7ae25de667c9e31901d76be62
Noah <noa316@naver.com>
2021-01-07 03:59:28 +0900

    Test class in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Noah",
-    age: 24,
-    gender: "male"
-};
+const Noah = new Human("Noah", 24, "male");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(Noah));
 
 export {};
