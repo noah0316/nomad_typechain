fa1bc5d5f31a5c0aae500c2ee1d05b2532a0873f
Noah <noa316@naver.com>
2021-01-07 03:52:02 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-const name = "Noah",
-    age = 24,
-    gender = "male";
-
-const sayHi = (name, age, gender?) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
 }
 
-sayHi(name, age);
+const person = {
+    name: "Noah",
+    age: 24,
+    gender: "male"
+};
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+console.log(sayHi(person));
 
 export {};
